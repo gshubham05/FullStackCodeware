9515179fb598c05942c12bb42cba32f37a592ecd
Deepka503200 <deepaksinghchauhan914@gmail.com>
2025-02-21 14:46:16 +0530

    error solve

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Herosection from "./Components/Herosection";
 import Aboutcompany from "./Components/Aboutcompany";
-import Faq from "./Components/faq";
+import Faq from "./Components/Faq";
 import Courses from "./Components/Courses";
 import Cstatistics from "./Components/Cstatistics";
 function page() {
